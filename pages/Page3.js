d38dc319d06079848d37b34a93f0bfe855620861
TranMinhHuy2003 <bosstran1212@gmail.com>
2024-09-26 09:23:51 +0700

    stack navigator

---
+++ pages/Page3.js
@@ -0,0 +1,13 @@
+import { View, Text,Image } from 'react-native'
+import React from 'react'
+
+export default function PageCtHoa({ navigation,route }) {
+  var hoachon=route.params.hoa;
+  return (
+    <View>
+      <Text>Tên Hoa :{hoachon.tenhoa}</Text>
+      <Image source={hoachon.hinh}  />
+      <Text>Tên Hoa {hoachon.mota}</Text>
+    </View>
+  )
+}
