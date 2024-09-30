f2d74c589f1e59e32471890bc81d93c598aff382
TranMinhHuy2003 <bosstran1212@gmail.com>
2024-09-30 23:29:08 +0700

    animated splash screen with zoom effect

--- App.js
+++ App.js
@@ -1,48 +1,54 @@
-// Example of Animated Splash Screen in React Native
-// https://aboutreact.com/animated-splash-screen/
+// Example of Animation Splash Screen with Zoom Effect
+// https://aboutreact.com/animation-splash-screen-with-zoom-effect/
 
-// import React in our code
-import React, {useState, useEffect} from 'react';
-
-// import all the components we are going to use
-import {View, Text, Image, StyleSheet} from 'react-native';
+// Import React
+import React, {useEffect} from 'react';
+// Import required components
+import {View, Text, StyleSheet, Image, Animated} from 'react-native';
 
 const App = () => {
-  const [align, setAlign] = useState('center');
-  const [alignsecond, setAlignsecond] = useState(false);
+  const width = new Animated.Value(0);
+  const height = new Animated.Value(600);
+  const SITE_BANNER_VERTICAL_IMAGE =
+    'https://raw.githubusercontent.com/AboutReact/sampleresource/master/site_banner_vertical.png';
+  const SAMPLE_APP_LOGO =
+    'https://raw.githubusercontent.com/AboutReact/sampleresource/master/react_logo.png';
 
   useEffect(() => {
-    let myTimeout = setTimeout(() => {
-      setAlign('flex-start'), setAlignsecond(true);
-    }, 3000);
-    return () => clearTimeout(myTimeout);
+    Animated.timing(
+      width, // The animated value to drive
+      {
+        toValue: 360, // Animate to opacity: 1 (opaque)
+        duration: 650, // Make it take a while
+        useNativeDriver: false,
+      },
+    ).start(); // Starts the animation
+    Animated.timing(
+      height, // The animated value to drive
+      {
+        toValue: 750, // Animate to opacity: 1 (opaque)
+        duration: 10000, // Make it take a while
+        useNativeDriver: false,
+      },
+    ).start(); // Starts the animation
   }, []);
 
   return (
-    <View
-      style={[
-        styles.container,
-        {justifyContent: align}
-      ]}>
-      <Image
-        source={{
-          uri:
-            'https://raw.githubusercontent.com/AboutReact/sampleresource/master/react_logo.png',
+    <View style={styles.container}>
+      <Animated.Image
+        source={{uri: SITE_BANNER_VERTICAL_IMAGE}}
+        style={{
+          width: width,
+          height: height,
+          position: 'absolute',
         }}
-        style={{width: 100, height: 100}}
       />
-      {!alignsecond ? null : (
-        <View style={{margin: 10}}>
-          <Text
-            style={{
-              color: '#114998',
-              fontSize: 30,
-              fontWeight: 'bold',
-            }}>
-            About React
-          </Text>
-        </View>
-      )}
+      <View style={styles.logoContainer}>
+        <Image source={{uri: SAMPLE_APP_LOGO}} style={styles.logo} />
+        <Text style={styles.textStyle}>
+          Example of Animation Splash Screen with Zoom Effect
+        </Text>
+      </View>
     </View>
   );
 };
@@ -52,8 +58,22 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    position: 'relative',
+    backgroundColor: '#2F7ECC',
+  },
+  logoContainer: {
+    flex: 1,
     alignItems: 'center',
-    flexDirection: 'row',
-    marginHorizontal: 40,
+    paddingTop: 48,
+    backgroundColor: 'rgba(11, 56, 82, 0.3)',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  textStyle: {
+    fontSize: 18,
+    color: 'white',
+    textAlign: 'center',
   },
 });
